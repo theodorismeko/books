f63cce13984e861ea3d72bcbbd73dd79d20cb716
Theodoris Meko <56846538+theodorismeko@users.noreply.github.com>
2024-01-18 17:28:30 +0200

    delete book by id functionality added

--- src/App.js
+++ src/App.js
@@ -5,6 +5,14 @@ import BookList from './components/BookList';
 function App() {
     const [books, setBooks] = useState([]);
 
+    const deleteBookById = (id) => {
+        const updatedBooks = books.filter((book) => {
+            return book.id !== id;
+        });
+
+        setBooks(updatedBooks);
+    };
+
     const createBook = (title) => {
         const updateBooks = [
             ...books, 
@@ -18,7 +26,7 @@ function App() {
 
     return (
         <div className="app">
-            <BookList books={books} />
+            <BookList books={books} onDelete={deleteBookById}/>
             <BookCreate onCreate={createBook} />
         </div>
     );
